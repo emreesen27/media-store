abff9f5beda15748ed8bbc58aa72e428013c72b2
Emre Esen <emre@Emres-MacBook-Pro.local>
2024-05-25 15:07:23 +0300

    [DEV][order strategy added]

--- src/main/java/com/sn/mediastorepv/data/MediaSelectionData.kt
+++ src/main/java/com/sn/mediastorepv/data/MediaSelectionData.kt
@@ -1,9 +1,7 @@
 package com.sn.mediastorepv.data
 
-import android.provider.MediaStore
-
 data class MediaSelectionData(
     val selection: String? = null,
     val selectionArgs: List<String>? = null,
-    val sortOrder: String? = "${MediaStore.MediaColumns.DATE_ADDED} DESC"
+    val sortOrder: String? = OrderStrategy.dateModified(OrderStrategy.DESC)
 )
---
+++ src/main/java/com/sn/mediastorepv/data/OrderStrategy.kt
@@ -0,0 +1,11 @@
+package com.sn.mediastorepv.data
+
+import androisdd.provider.MediaStore
+
+object OrderStrategy {
+    const val DESC = "DESC"
+    const val ASC = "ASC"
+    fun dateModified(sortBy: String) = "${MediaStore.MediaColumns.DATE_MODIFIED} $sortBy"
+    fun name(sortBy: String) = "${MediaStore.MediaColumns.DISPLAY_NAME} $sortBy"
+
+}
