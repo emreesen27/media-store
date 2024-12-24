738a3cce61124c9f6a77a7b6da51f81d2cdcb783
Emre Esen <emre@Emres-MacBook-Pro.local>
2024-12-24 17:20:11 +0300

    [DEV][extension order added]

--- src/main/java/com/sn/mediastorepv/data/OrderStrategy.kt
+++ src/main/java/com/sn/mediastorepv/data/OrderStrategy.kt
@@ -5,7 +5,11 @@ import android.provider.MediaStore
 object OrderStrategy {
     const val DESC = "DESC"
     const val ASC = "ASC"
+
     fun dateModified(sortBy: String) = "${MediaStore.MediaColumns.DATE_MODIFIED} $sortBy"
+
     fun name(sortBy: String) = "${MediaStore.MediaColumns.DISPLAY_NAME} $sortBy"
+
+    fun extension(sortBy: String) = "${MediaStore.MediaColumns.MIME_TYPE} $sortBy"
 
 }
